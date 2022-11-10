94dfd2898e8fa54cfa29b3f51d9d98c9fce6e9df
“Daniel” <daniel.fabian13x@gmail.com>
2022-11-10 18:43:48 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+import { AuthGuard, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard'
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login'])
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  },
+  {
+    path: 'main',
+    component: MainComponent,
+    // canActivate: [AuthGuard],
+    // data: { authGuardPipe: redirectUnauthorizedToLogin }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'proyecto-integrado-65cf2',
+    appId: '1:176525818470:web:1fc00d5a05978028600ba7',
+    storageBucket: 'proyecto-integrado-65cf2.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyC3jENymyjrKsqyitqE8-WyuK5fPPCMKrA',
+    authDomain: 'proyecto-integrado-65cf2.firebaseapp.com',
+    messagingSenderId: '176525818470',
+  },
   production: false
 };
 
